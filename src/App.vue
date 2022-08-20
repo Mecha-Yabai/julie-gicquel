059aef88b85d1caf82a279f10d0d4cb0f70094ca
Julie Gicquel <Julie-Gicquel_student2018@wilder.school>
2022-08-20 11:25:09 +0200

    Main app & testing back to top

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,41 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div :class="mainTheme">
+    <!-- App header -->
+    <MainHeader class="container mx-auto px-4 sm:px-16" />
+
+    <!-- Render active component contents with vue transition -->
+    <transition name="fade" mode="out-in">
+      <router-view :theme="mainTheme" />
+    </transition>
+
+    <!-- App footer -->
+    <MainFooter />
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import MainHeader from "./components/main/MainHeader.vue";
+import MainFooter from "./components/main/MainFooter.vue";
+import feather from "feather-icons";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    MainHeader,
+    MainFooter,
+  },
+  data: () => {
+    return {
+      mainTheme: localStorage.getItem("theme"),
+    };
+  },
+  mounted() {
+    feather.replace();
+  },
+  updated() {
+    feather.replace();
+  },
+};
 </script>
 
 <style>
@@ -20,7 +44,49 @@ export default {
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+}
+
+.vue-back-to-top {
+  @apply p-2 bg-red-500 hover:bg-red-600 text-white transition
+        duration-500
+        ease-in-out
+        transform
+        hover:-translate-y-1 hover:scale-110;
+  border-radius: 50%;
+  font-size: 22px;
+  line-height: 22px;
+}
+
+.fade-enter-active {
+  animation: coming 0.4s;
+  animation-delay: 0.2s;
+  opacity: 0;
+}
+
+.fade-leave-active {
+  animation: going 0.4s;
+}
+
+@keyframes going {
+  from {
+    transform: translateX(0);
+  }
+
+  to {
+    transform: translateX(-10px);
+    opacity: 0;
+  }
+}
+
+@keyframes coming {
+  from {
+    transform: translateX(-10px);
+    opacity: 0;
+  }
+
+  to {
+    transform: translateX(0px);
+    opacity: 1;
+  }
 }
 </style>
