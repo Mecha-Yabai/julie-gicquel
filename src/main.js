46d656a5c19847b1c20fcf7555df13ebc100ccd2
Julie Gicquel <Julie-Gicquel_student2018@wilder.school>
2022-08-20 11:24:11 +0200

    Lottie & main theme

--- src/main.js
+++ src/main.js
@@ -1,4 +1,22 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from "vue";
+import App from "./App.vue";
+import router from "./router";
+import "./assets/css/app.css";
+import BackToTop from "vue-backtotop";
 
-createApp(App).mount('#app')
+const feather = require("feather-icons");
+feather.replace();
+
+createApp(App).use(router).use(BackToTop).mount("#app");
+
+const mainTheme = localStorage.getItem("theme");
+
+// Check what is the active theme and change theme when user clicks on the theme button in header.
+if (
+  mainTheme === "dark" &&
+  document.querySelector("body").classList.contains("main-theme")
+) {
+  document.querySelector("body").classList.add("bg-primary-dark");
+} else {
+  document.querySelector("body").classList.add("bg-secondary-light");
+}
