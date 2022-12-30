1d82e3c3beaae955b382c845504574f32b6884cb
Julie Gicquel <Julie-Gicquel_student2018@wilder.school>
2022-12-30 19:45:48 +0100

    Delete old code / UI UX locale switcher

--- src/components/LocaleSwitcher.vue
+++ src/components/LocaleSwitcher.vue
@@ -1,9 +1,80 @@
 <template>
-  <select v-model="$i18n.locale">
-    <option v-for="(locale, i) in locales" :key="`locale-${i}`" :value="locale">
-      {{ locale }}
-    </option>
-  </select>
+  <!-- <div class="w-36">
+    <select
+      class="px-3 py-2 bg-white border focus:ring-red-500 focus:border-red-500 border-gray-500 rounded-lg w-full"
+      v-model="$i18n.locale"
+    >
+      <option
+        v-for="(locale, i) in locales"
+        :key="`locale-${i}`"
+        :value="locale.id"
+      >
+        <img
+          :src="locale.flag"
+          class="w-4"
+          :alt="locale.name"
+        />
+        {{ locale.name }}
+      </option>
+    </select>
+  </div> -->
+  <div class="relative text-lg w-12 md:w-18 lg:w-36">
+    <button
+      class="flex items-center justify-between px-3 py-2 bg-white w-full border border-gray-500 rounded-lg"
+      @click="isOptionsExpanded = !isOptionsExpanded"
+      @blur="isOptionsExpanded = false"
+    >
+      <span class="hidden lg:block">{{ selectedLocale.name }}</span>
+      <img class="lg:hidden lg:w-auto lg:h-5 py-1" :src="selectedLocale.flag" />
+      <svg
+        fill="none"
+        viewBox="0 0 24 24"
+        stroke="currentColor"
+        class="h-4 w-4 transform transition-transform duration-200 ease-in-out"
+        :class="isOptionsExpanded ? 'rotate-180' : 'rotate-0'"
+      >
+        <path
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          stroke-width="2"
+          d="M19 9l-7 7-7-7"
+        />
+      </svg>
+    </button>
+    <transition
+      enter-active-class="transform transition duration-500 ease-custom"
+      enter-class="-translate-y-1/2 scale-y-0 opacity-0"
+      enter-to-class="translate-y-0 scale-y-100 opacity-100"
+      leave-active-class="transform transition duration-300 ease-custom"
+      leave-class="translate-y-0 scale-y-100 opacity-100"
+      leave-to-class="-translate-y-1/2 scale-y-0 opacity-0"
+    >
+      <ul
+        v-show="isOptionsExpanded"
+        class="absolute left-0 right-0 mb-4 bg-white divide-y rounded-lg shadow-lg overflow-hidden"
+      >
+        <li
+          v-for="locale in locales"
+          :key="locale.id"
+          class="px-3 py-3 transition-colors duration-300 hover:bg-gray-200"
+          @mousedown.prevent="setOption(locale)"
+        >
+          <div
+            class="flex justify-center lg:justify-start lg:items-center lg:px-1"
+          >
+            <img
+              class="w-12 lg:w-auto lg:h-5"
+              :src="locale.flag"
+              :alt="locale.name"
+            />
+            <span class="hidden lg:ml-2 lg:block font-jiho-light">{{
+              locale.name
+            }}</span>
+          </div>
+        </li>
+      </ul>
+    </transition>
+  </div>
 </template>
 <script>
 import { watch } from "vue";
@@ -23,7 +94,61 @@ export default {
     };
   },
   data() {
-    return { locales: ["fr", "en", "ja"] };
+    return {
+      isOptionsExpanded: false,
+      locales: [
+        {
+          id: "fr",
+          name: "Français",
+          flag: require("@/assets/images/france.svg"),
+        },
+        { id: "en", name: "English", flag: require("@/assets/images/uk.svg") },
+        {
+          id: "ja",
+          name: "日本語",
+          flag: require("@/assets/images/japon.svg"),
+        },
+      ],
+    };
+  },
+  methods: {
+    setOption(locale) {
+      this.$i18n.locale = locale.id;
+      this.selectedLocale = locale.name;
+      this.isOptionsExpanded = false;
+    },
+  },
+  computed: {
+    selectedLocale: {
+      get() {
+        let lang = {};
+        const locale = this.$i18n.locale;
+        switch (locale) {
+          case "fr":
+            lang = {
+              name: "Français",
+              flag: require("@/assets/images/france.svg"),
+            };
+            break;
+          case "en":
+            lang = { name: "English", flag: require("@/assets/images/uk.svg") };
+            break;
+          case "ja":
+            lang = {
+              name: "日本語",
+              flag: require("@/assets/images/japon.svg"),
+            };
+            break;
+
+          default:
+            break;
+        }
+        return lang;
+      },
+      set(value) {
+        return value;
+      },
+    },
   },
 };
 </script>
--- src/components/main/MainHeader.vue
+++ src/components/main/MainHeader.vue
@@ -21,38 +21,12 @@
           </router-link>
         </div>
         <div class="flex items-end">
+          <locale-switcher class="block md:hidden mr-2" />
           <theme-switcher
             :theme="theme"
             @themeChanged="updateTheme"
             class="block md:hidden bg-ternary-light text-red-600 dark:bg-ternary-dark hover:bg-hover-light dark:hover:bg-hover-dark hover:shadow-sm px-2.5 py-2 mr-2 rounded-lg"
           />
-
-          <div class="md:hidden">
-            <button
-              @click="isOpen = !isOpen"
-              type="button"
-              class="focus:outline-none"
-              aria-label="Hamburger Menu"
-            >
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 24 24"
-                class="h-7 w-7 fill-current text-secondary-dark dark:text-ternary-light"
-              >
-                <path
-                  v-if="isOpen"
-                  fill-rule="evenodd"
-                  d="M18.278 16.864a1 1 0 0 1-1.414 1.414l-4.829-4.828-4.828 4.828a1 1 0 0 1-1.414-1.414l4.828-4.829-4.828-4.828a1 1 0 0 1 1.414-1.414l4.829 4.828 4.828-4.828a1 1 0 1 1 1.414 1.414l-4.828 4.829 4.828 4.828z"
-                  clip-rule="evenodd"
-                ></path>
-                <path
-                  v-if="!isOpen"
-                  fill-rule="evenodd"
-                  d="M4 5h16a1 1 0 0 1 0 2H4a1 1 0 1 1 0-2zm0 6h16a1 1 0 0 1 0 2H4a1 1 0 0 1 0-2zm0 6h16a1 1 0 0 1 0 2H4a1 1 0 0 1 0-2z"
-                ></path>
-              </svg>
-            </button>
-          </div>
         </div>
       </div>
 
@@ -126,10 +100,3 @@ export default {
   },
 };
 </script>
-<style scoped>
-#nav a.router-link-exact-active {
-  @apply text-red-700;
-  @apply dark:text-red-400;
-  @apply font-medium;
-}
-</style>
