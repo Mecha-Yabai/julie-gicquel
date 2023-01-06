ed0bd988154914105cf79a2a182f3f63a81bf028
Julie Gicquel <Julie-Gicquel_student2018@wilder.school>
2023-01-06 16:20:42 +0100

    Edit socials & footer

--- src/components/main/MainFooter.vue
+++ src/components/main/MainFooter.vue
@@ -7,14 +7,21 @@
         <p
           class="font-jiho-bold text-3xl sm:text-4xl font-semibold text-primary-dark dark:text-primary-light mb-5"
         >
-          Suivez-moi
+          Contactez-moi sur
         </p>
         <ul class="flex gap-4 sm:gap-8">
           <SocialButton />
+          <a
+            :href="`mailto:${mail}`"
+            target="__blank"
+            class="rounded-full h-11 w-11 sm:h-12 sm:w-12 inline-flex items-center justify-center mx-2 bg-white dark:bg-blue-dark shadow-lg border-primary-dark dark:border-blue-dark border-2 hover:bg-primary-dark text-primary-dark dark:text-white dark:hover:bg-blue-dark dark:hover:text-red-dark hover:text-white text-center"
+          >
+            <i data-feather="mail" class="w-5 lg:w-16"></i>
+          </a>
         </ul>
       </div>
 
-      <MainFooterCopyright />
+      <MainFooterCopyright :mail="mail" />
     </div>
   </div>
 </template>
@@ -24,5 +31,10 @@ import MainFooterCopyright from "./MainFooterCopyright.vue";
 
 export default {
   components: { MainFooterCopyright, SocialButton },
+  data() {
+    return {
+      mail: "julie.gcql@gmail.com",
+    };
+  },
 };
 </script>
