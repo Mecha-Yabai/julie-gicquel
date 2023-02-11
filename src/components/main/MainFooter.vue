20dd19b833992ca0d1dcc28697bf757be409033e
Julie Gicquel <julie.gcql@gmail.com>
2023-02-11 12:21:19 +0100

    Go to top bottom

--- src/components/main/MainFooter.vue
+++ src/components/main/MainFooter.vue
@@ -1,13 +1,43 @@
 <template>
   <div class="container mx-auto">
     <div
-      class="pt-20 sm:pt-30 pb-8 mt-20 border-t-2 border-primary-light dark:border-secondary-dark"
+      class="pt-16 pb-8 mt-20 border-t-2 border-primary-light dark:border-secondary-dark"
     >
+      <div class="pb-5 flex justify-center">
+        <button @click="goTop()">
+          <div class="text text-primary-dark">
+            <span class="font-jiho-regular">{{ $t("footer.back_to_top_1") }}</span>
+            <span class="font-jiho-regular">{{ $t("footer.back_to_top_2") }}</span>
+            <span class="font-jiho-regular">{{ $t("footer.back_to_top_3") }}</span>
+          </div>
+          <div class="clone text-primary-dark">
+            <span class="font-jiho-regular">{{ $t("footer.back_to_top_1") }}</span>
+            <span class="font-jiho-regular">{{ $t("footer.back_to_top_2") }}</span>
+            <span class="font-jiho-regular">{{ $t("footer.back_to_top_3") }}</span>
+          </div>
+          <svg
+            width="20px"
+            xmlns="http://www.w3.org/2000/svg"
+            class="h-6 w-6"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="#030303"
+            stroke-width="2"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              d="M14 5l7 7m0 0l-7 7m7-7H3"
+              stroke="#030303"
+            ></path>
+          </svg>
+        </button>
+      </div>
       <div class="flex flex-col justify-center items-center mb-12 sm:mb-20">
         <p
           class="font-jiho-bold text-3xl sm:text-4xl font-semibold text-primary-dark dark:text-primary-light mb-5"
         >
-          {{ $t("contact_me") }}
+          {{ $t("footer.contact_me") }}
         </p>
         <ul class="flex gap-4 sm:gap-8">
           <SocialButton />
@@ -36,5 +66,103 @@ export default {
       mail: "julie.gcql@gmail.com",
     };
   },
+  methods: {
+    goTop: function () {
+        window.scrollTo({
+          top: 0,
+          behavior: "smooth"
+        });
+      },
+  },
 };
 </script>
+<style lang="css" scoped>
+button {
+  width: 140px;
+  height: 56px;
+  overflow: hidden;
+  border: none;
+  color: #030303;
+  background: none;
+  position: relative;
+  padding-bottom: 2em;
+}
+
+button > div,
+button > svg {
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  display: flex;
+}
+
+button:before {
+  content: "";
+  position: absolute;
+  height: 2px;
+  bottom: 0;
+  left: 0;
+  width: 100%;
+  transform: scaleX(0);
+  transform-origin: bottom right;
+  background: currentColor;
+  transition: transform 0.25s ease-out;
+}
+
+button:hover:before {
+  transform: scaleX(1);
+  transform-origin: bottom left;
+}
+
+button .clone > *,
+button .text > * {
+  opacity: 1;
+  font-size: 1.3rem;
+  transition: 0.2s;
+  margin-left: 4px;
+}
+
+button .clone > * {
+  transform: translateY(60px);
+}
+
+button:hover .clone > * {
+  opacity: 1;
+  transform: translateY(0px);
+  transition: all 0.2s cubic-bezier(0.215, 0.61, 0.355, 1) 0s;
+}
+
+button:hover .text > * {
+  opacity: 1;
+  transform: translateY(-60px);
+  transition: all 0.2s cubic-bezier(0.215, 0.61, 0.355, 1) 0s;
+}
+
+button:hover .clone > :nth-child(1) {
+  transition-delay: 0.15s;
+}
+
+button:hover .clone > :nth-child(2) {
+  transition-delay: 0.2s;
+}
+
+button:hover .clone > :nth-child(3) {
+  transition-delay: 0.25s;
+}
+
+button:hover .clone > :nth-child(4) {
+  transition-delay: 0.3s;
+}
+/* icon style and hover */
+button svg {
+  width: 20px;
+  right: 0;
+  top: 50%;
+  transform: translateY(-50%) rotate(-50deg);
+  transition: 0.2s ease-out;
+}
+
+button:hover svg {
+  transform: translateY(-50%) rotate(-90deg);
+}
+</style>
