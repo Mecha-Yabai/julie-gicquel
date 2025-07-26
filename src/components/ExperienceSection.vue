23efb4729ce566addab8bfec2c04155ec45233db
Julie <julie.gcql@gmail.com>
2025-07-26 13:43:19 +0200

    Udpates dates / translations / fix lint

--- src/components/ExperienceSection.vue
+++ src/components/ExperienceSection.vue
@@ -1,231 +1,221 @@
 <template>
-    <div class="flex flex-col lg:justify-between lg:flex-row inherit-height">
-        <div class="w-full lg:pr-6 lg:w-6/12 flex flex-col">
-            <h1
-                class="font-jiho-medium lg:text-5xl sm:text-4xl text-3xl text-center lg:text-left text-dark dark:text-grey-lighter my-4"
-            >
-                {{ $t("experiences.title_1") }}
-                <span class="text-red-500 dark:text-primary">{{
-                    $t("experiences.title_2")
-                }}</span>
-                {{ $t("experiences.title_3") }}
-            </h1>
-            <div class="space-y-4 my-4">
-                <p
-                    v-for="paragraph in experiencesParagraphs"
-                    :key="paragraph.id"
-                    class="text-center lg:text-left font-jiho-regular text-lg lg:text-xl text-gray-500 dark:text-grey-light"
-                >
-                    {{ $t(paragraph.text) }}
-                </p>
-            </div>
-            <div class="hidden h-full lg:flex items-center">
-                <img
-                    v-if="theme === 'light'"
-                    src="@/assets/images/hero1_LIGHT.png"
-                    alt="Developer"
-                    class="lg:w-full"
-                />
-                <img
-                    v-else
-                    src="@/assets/images/hero1_LIGHT_darkmode.png"
-                    alt="Developer"
-                    class="lg:w-full"
-                />
-            </div>
-        </div>
-        <div
-            class="w-full flex-1 lg:pl-6 lg:w-6/12 flex flex-col items-baseline h-full justify-start lg:justify-center lg:self-center mt-6 md:mt-12 lg:mt-0"
+  <div class="flex flex-col lg:justify-between lg:flex-row inherit-height">
+    <div class="w-full lg:pr-6 lg:w-6/12 flex flex-col">
+      <h1
+        class="font-jiho-medium lg:text-5xl sm:text-4xl text-3xl text-center lg:text-left text-dark dark:text-grey-lighter my-4"
+      >
+        {{ $t("experiences.title_1") }}
+        <span class="text-red-500 dark:text-primary">{{
+          $t("experiences.title_2")
+        }}</span>
+        {{ $t("experiences.title_3") }}
+      </h1>
+      <div class="space-y-4 my-4">
+        <p
+          v-for="paragraph in experiencesParagraphs"
+          :key="paragraph.id"
+          class="text-center lg:text-left font-jiho-regular text-lg lg:text-xl text-gray-500 dark:text-grey-light"
         >
-            <template v-for="experience in experiences" :key="experience.id">
-                <div
-                    class="flex items-baseline w-full justify-between h-[110px] lg:h-[140px] border-t border-dark dark:border-secondary"
-                >
-                    <div class="hidden md:block my-auto w-[70px] md:w-[100px]">
-                        <time
-                            class="text-sm font-jiho-regular text-gray-500 dark:text-grey-light"
-                            >{{ experience.date }}</time
-                        >
-                    </div>
-                    <div class="my-auto flex-1 pr-4">
-                        <h3
-                            class="text-md lg:text-lg font-jiho-medium text-dark dark:text-white"
-                        >
-                            {{ experience.name }}
-                        </h3>
-                        <h5 class="mt-2 text-sm font-jiho-regular text-primary">
-                            {{ experience.company }}
-                        </h5>
-                        <time
-                            class="md:hidden text-sm font-jiho-regular text-gray-500 dark:text-grey-light"
-                            >{{ experience.date }}</time
-                        >
-                    </div>
-                    <div class="my-auto">
-                        <button
-                            type="button"
-                            @click="openExperienceInfosPanel(experience)"
-                            class="rounded-full p-2 text-dark hover:text-primary dark:text-white dark:hover:text-primary"
-                        >
-                            <i
-                                data-feather="plus-circle"
-                                stroke-width="1.5"
-                            ></i>
-                        </button>
-                    </div>
-                </div>
-            </template>
-        </div>
+          {{ $t(paragraph.text) }}
+        </p>
+      </div>
+      <div class="hidden h-full lg:flex items-center">
+        <img
+          v-if="theme === 'light'"
+          src="@/assets/images/hero1_LIGHT.png"
+          alt="Developer"
+          class="lg:w-full"
+        />
+        <img
+          v-else
+          src="@/assets/images/hero1_LIGHT_darkmode.png"
+          alt="Developer"
+          class="lg:w-full"
+        />
+      </div>
     </div>
-    <transition
-        name="panel-fade"
-        ref="experienceInfosPanel"
-        tabindex="-1"
-        v-show="isExperienceInfosPanelOpen"
-        @keydown.escape="closeExperienceInfosPanel()"
-        class="fixed inset-y-0 right-0 z-20 w-full max-w-xs bg-white dark:bg-dark shadow-xl dark:bg-darker dark:text-light sm:max-w-md focus:outline-none"
+    <div
+      class="w-full flex-1 lg:pl-6 lg:w-6/12 flex flex-col items-baseline h-full justify-start lg:justify-center lg:self-center mt-6 md:mt-12 lg:mt-0"
     >
+      <template v-for="experience in experiences" :key="experience.id">
         <div
-            class="flex flex-col h-screen dark:border-l-2 dark:border-secondary"
+          class="flex items-baseline w-full justify-between h-[110px] lg:h-[140px] border-t border-dark dark:border-secondary"
         >
-            <div
-                class="flex tems-center justify-center px-4 py-6 border-b dark:border-secondary"
+          <div class="hidden md:block my-auto w-[70px] md:w-[100px]">
+            <time
+              class="text-sm font-jiho-regular text-gray-500 dark:text-grey-light"
+              >{{ experience.date }}</time
             >
-                <h3
-                    class="flex-1 text-md lg:text-lg font-jiho-medium text-primary"
-                >
-                    {{ currentExperience.name }}
-                </h3>
-                <button
-                    @click="closeExperienceInfosPanel()"
-                    class="flex-none text-primary ml-2"
-                >
-                    <i data-feather="x"></i>
-                </button>
-            </div>
-            <div class="flex-1 overflow-y-auto pt-2 pb-6">
-                <div class="px-6 py-2" v-if="currentExperience.description">
-                    <h5
-                        class="mt-2 font-jiho-regular text-md text-gray-500 dark:text-grey-light"
-                    >
-                        {{ currentExperience.description }}
-                    </h5>
-                </div>
-                <div
-                    v-if="currentExperience.duties"
-                    class="px-6 py-2 font-jiho-medium text-primay-dark dark:text-white"
-                >
-                    {{ $t("experiences.duties") }}
-                </div>
-                <template
-                    v-for="duty in currentExperience.duties"
-                    :key="duty.id"
-                >
-                    <div
-                        class="px-6 py-2 font-jiho-regular text-primay-dark dark:text-white"
-                    >
-                        {{ $t(duty) }}
-                    </div>
-                </template>
-                <div
-                    v-if="currentExperience.stacks"
-                    class="px-6 py-2 font-jiho-medium text-primay-dark dark:text-white"
-                >
-                    {{ $t("experiences.stack") }}
-                </div>
-                <template
-                    v-for="stack in currentExperience.stacks"
-                    :key="stack.id"
-                >
-                    <div
-                        class="px-6 py-2 font-jiho-regular text-primay-dark dark:text-white"
-                    >
-                        {{ stack }}
-                    </div>
-                </template>
-            </div>
+          </div>
+          <div class="my-auto flex-1 pr-4">
+            <h3
+              class="text-md lg:text-lg font-jiho-medium text-dark dark:text-white"
+            >
+              {{ experience.name }}
+            </h3>
+            <h5 class="mt-2 text-sm font-jiho-regular text-primary">
+              {{ experience.company }}
+            </h5>
+            <time
+              class="md:hidden text-sm font-jiho-regular text-gray-500 dark:text-grey-light"
+              >{{ experience.date }}</time
+            >
+          </div>
+          <div class="my-auto">
+            <button
+              type="button"
+              @click="openExperienceInfosPanel(experience)"
+              class="rounded-full p-2 text-dark hover:text-primary dark:text-white dark:hover:text-primary"
+            >
+              <i data-feather="plus-circle" stroke-width="1.5"></i>
+            </button>
+          </div>
         </div>
-    </transition>
+      </template>
+    </div>
+  </div>
+  <transition
+    name="panel-fade"
+    ref="experienceInfosPanel"
+    tabindex="-1"
+    @keydown.escape="closeExperienceInfosPanel()"
+  >
+    <div
+      v-show="isExperienceInfosPanelOpen"
+      class="fixed inset-y-0 right-0 z-20 w-full max-w-xs bg-white dark:bg-dark shadow-xl dark:bg-darker dark:text-light sm:max-w-md focus:outline-none flex flex-col h-screen dark:border-l-2 dark:border-secondary"
+      role="dialog"
+      aria-modal="true"
+    >
+      <div
+        class="flex tems-center justify-center px-4 py-6 border-b dark:border-secondary"
+      >
+        <h3 class="flex-1 text-md lg:text-lg font-jiho-medium text-primary">
+          {{ currentExperience.name }}
+        </h3>
+        <button
+          @click="closeExperienceInfosPanel()"
+          class="flex-none text-primary ml-2"
+        >
+          <i data-feather="x"></i>
+        </button>
+      </div>
+      <div class="flex-1 overflow-y-auto pt-2 pb-6">
+        <div class="px-6 py-2" v-if="currentExperience.description">
+          <h5
+            class="mt-2 font-jiho-regular text-md text-gray-500 dark:text-grey-light"
+          >
+            {{ currentExperience.description }}
+          </h5>
+        </div>
+        <div
+          v-if="currentExperience.duties"
+          class="px-6 py-2 font-jiho-medium text-primay-dark dark:text-white"
+        >
+          {{ $t("experiences.duties") }}
+        </div>
+        <template v-for="duty in currentExperience.duties" :key="duty.id">
+          <div
+            class="px-6 py-2 font-jiho-regular text-primay-dark dark:text-white"
+          >
+            {{ $t(duty) }}
+          </div>
+        </template>
+        <div
+          v-if="currentExperience.stacks"
+          class="px-6 py-2 font-jiho-medium text-primay-dark dark:text-white"
+        >
+          {{ $t("experiences.stack") }}
+        </div>
+        <template v-for="stack in currentExperience.stacks" :key="stack.id">
+          <div
+            class="px-6 py-2 font-jiho-regular text-primay-dark dark:text-white"
+          >
+            {{ stack }}
+          </div>
+        </template>
+      </div>
+    </div>
+  </transition>
 </template>
 <script>
-import { getExperiences } from "../data/experiences";
+  import { getExperiences } from "../data/experiences";
 
-export default {
+  export default {
     data() {
-        return {
-            theme: "",
-            currentExperience: {},
-            isExperienceInfosPanelOpen: false,
-        };
+      return {
+        theme: "",
+        currentExperience: {},
+        isExperienceInfosPanelOpen: false,
+      };
     },
     created() {
-        this.theme = localStorage.getItem("theme") || "light";
+      this.theme = localStorage.getItem("theme") || "light";
     },
     watch: {
-        isExperienceInfosPanelOpen: function () {
-            if (this.isExperienceInfosPanelOpen) {
-                document.documentElement.style.overflow = "hidden";
-                return;
-            }
+      isExperienceInfosPanelOpen: function () {
+        if (this.isExperienceInfosPanelOpen) {
+          document.documentElement.style.overflow = "hidden";
+          return;
+        }
 
-            document.documentElement.style.overflow = "auto";
-        },
+        document.documentElement.style.overflow = "auto";
+      },
     },
     computed: {
-        experiences() {
-            return getExperiences().map((experiences) => ({
-                ...experiences,
-                name: this.$t(experiences.name),
-                company: this.$t(experiences.company),
-                date: this.$t(experiences.date),
-            }));
-        },
-        experiencesParagraphs() {
-            return [
-                { id: 1, text: "experiences.description_1" },
-                { id: 2, text: "experiences.description_2" },
-                { id: 3, text: "experiences.description_3" },
-            ];
-        },
+      experiences() {
+        return getExperiences().map((experiences) => ({
+          ...experiences,
+          name: this.$t(experiences.name),
+          company: this.$t(experiences.company),
+          date: this.$t(experiences.date),
+        }));
+      },
+      experiencesParagraphs() {
+        return [
+          { id: 1, text: "experiences.description_1" },
+          { id: 2, text: "experiences.description_2" },
+          { id: 3, text: "experiences.description_3" },
+        ];
+      },
     },
     methods: {
-        closeExperienceInfosPanel() {
-            this.isExperienceInfosPanelOpen = false;
-        },
-        openExperienceInfosPanel(experience) {
-            this.currentExperience = {
-                ...experience,
-                description: experience.description
-                    ? this.$t(experience.description)
-                    : "",
-            };
-            this.isExperienceInfosPanelOpen = true;
-            this.$nextTick(() => {
-                this.$refs.experienceInfosPanel.focus();
-            });
-        },
+      closeExperienceInfosPanel() {
+        this.isExperienceInfosPanelOpen = false;
+      },
+      openExperienceInfosPanel(experience) {
+        this.currentExperience = {
+          ...experience,
+          description: experience.description
+            ? this.$t(experience.description)
+            : "",
+        };
+        this.isExperienceInfosPanelOpen = true;
+        this.$nextTick(() => {
+          this.$refs.experienceInfosPanel.focus();
+        });
+      },
     },
-};
+  };
 </script>
 <style lang="scss" scoped>
-.panel-fade-enter-active {
+  .panel-fade-enter-active {
     transition-duration: 500ms;
     transition-timing-function: cubic-bezier(0.4, 0, 0.2, 1);
-}
-.panel-fade-enter-from {
+  }
+  .panel-fade-enter-from {
     transform: translateX(100%);
-}
-.panel-fade-enter-to {
+  }
+  .panel-fade-enter-to {
     transform: translateX(0px);
-}
-.panel-fade-leave-active {
+  }
+  .panel-fade-leave-active {
     transition-duration: 500ms;
     transition-timing-function: cubic-bezier(0.4, 0, 0.2, 1);
-}
-.panel-fade-leave-from {
+  }
+  .panel-fade-leave-from {
     transform: translateX(0px);
-}
-.panel-fade-leave-to {
+  }
+  .panel-fade-leave-to {
     transform: translateX(100%);
-}
+  }
 </style>
--- src/components/HeroSection.vue
+++ src/components/HeroSection.vue
@@ -1,164 +1,163 @@
 <template>
+  <div
+    class="flex flex-col inherit-height pt-[72.66px] md:pt-[78px] lg:justify-between"
+  >
     <div
-        class="flex flex-col inherit-height pt-[72.66px] md:pt-[78px] lg:justify-between"
+      class="flex flex-1 flex-col-reverse lg:flex-row justify-center lg:items-center"
     >
-        <div
-            class="flex flex-1 flex-col-reverse lg:flex-row justify-center lg:items-center"
+      <div class="w-full lg:w-6/12 text-left lg:pr-8">
+        <h1
+          class="font-jiho-medium lg:text-5xl sm:text-4xl text-3xl text-center lg:text-left text-dark dark:text-grey-lighter my-4 md:my-8"
         >
-            <div class="w-full lg:w-6/12 text-left lg:pr-8">
-                <h1
-                    class="font-jiho-medium lg:text-5xl sm:text-4xl text-3xl text-center lg:text-left text-dark dark:text-grey-lighter my-4 md:my-8"
-                >
-                    {{ $t("greetings") }}
-                    <span
-                        class="text-red-500 dark:text-primary font-jiho-bold"
-                        >{{ $t("name") }}</span
-                    >,
-                    <br />
-                    {{ $t("web_developer") }}
-                </h1>
-                <div class="flex flex-col space-y-4">
-                    <p
-                        v-for="paragraph in heroParagraphs"
-                        :key="paragraph.id"
-                        class="text-center lg:text-justify font-jiho-regular text-lg lg:text-xl text-gray-500 dark:text-grey-light"
-                    >
-                        {{ $t(paragraph.text) }}
-                    </p>
-                </div>
-                <div
-                    class="flex items-center justify-evenly lg:justify-start my-4 sm:my-6 md:my-8"
-                >
-                    <div class="w-8/12 sm:w-1/2">
-                        <CTAButton
-                            :selectedIcon="'download'"
-                            :link="'/julie-gicquel/files/Julie-Gicquel-CV.pdf'"
-                            :download="'Julie-Gicquel-CV.pdf'"
-                            label="'CV'"
-                            class="bg-red-600 dark:bg-primary hover:bg-red-700 dark:hover:bg-primary-darker text-white"
-                        >
-                            {{ $t("download_resume") }}
-                        </CTAButton>
-                    </div>
-                    <div class="hidden sm:block sm:w-1/2 sm:ml-4">
-                        <CTAButton
-                            :selectedIcon="'mail'"
-                            :link="`mailto:${mail}`"
-                            label="'Contact'"
-                            class="bg-white dark:bg-secondary dark:hover:bg-secondary-darker border-dark dark:border-secondary dark:hover:border-secondary-darker border-2 hover:bg-dark text-dark dark:text-white dark:hover:text-primary hover:text-white"
-                        >
-                            {{ $t("get_in_touch") }}
-                        </CTAButton>
-                    </div>
-                </div>
-                <div class="flex flex-col space-y-2">
-                    <p
-                        class="font-jiho-medium text-lg xl:text-xl text-center lg:text-left text-primay-dark dark:text-white"
-                    >
-                        {{ $t("follow_me") }}
-                    </p>
-                    <div class="flex justify-center lg:justify-start">
-                        <SocialButton />
-                        <a
-                            :href="`mailto:${mail}`"
-                            class="inline-flex sm:hidden rounded-full h-11 w-11 sm:h-12 sm:w-12 items-center justify-center mx-2 bg-white dark:bg-secondary dark:hover:bg-secondary-darker border-dark dark:border-secondary dark:hover:border-secondary-darker border-2 hover:bg-dark text-dark dark:text-white dark:hover:text-primary hover:text-white text-center"
-                        >
-                            <i data-feather="mail" class="w-5 lg:w-16"></i>
-                        </a>
-                    </div>
-                </div>
-            </div>
-            <div class="w-full lg:w-6/12 flex justify-center lg:justify-end">
-                <img
-                    v-if="theme === 'light'"
-                    src="@/assets/images/hero3_LIGHT.png"
-                    alt="Developer"
-                    class="w-11/12 sm:w-7/12 lg:w-10/12"
-                />
-                <img
-                    v-else
-                    src="@/assets/images/hero3_LIGHT_darkmode.png"
-                    alt="Developer"
-                    class="w-11/12 sm:w-7/12 lg:w-10/12"
-                />
-            </div>
+          {{ $t("greetings") }}
+          <span class="text-red-500 dark:text-primary font-jiho-bold">{{
+            $t("name")
+          }}</span>
+          <br />
+          {{ $t("web_developer") }}
+          <span class="text-red-500 dark:text-primary">{{ $t("and") }}</span>
+          {{ $t("designer_ux") }}
+        </h1>
+        <div class="flex flex-col space-y-4">
+          <p
+            v-for="paragraph in heroParagraphs"
+            :key="paragraph.id"
+            class="text-center lg:text-justify font-jiho-regular text-lg lg:text-xl text-gray-500 dark:text-grey-light"
+          >
+            {{ $t(paragraph.text) }}
+          </p>
         </div>
         <div
-            class="hidden lg:flex lg:flex-1 flex-none items-center justify-center"
+          class="flex items-center justify-evenly lg:justify-start my-4 sm:my-6 md:my-8"
         >
-            <router-link :to="{ name: 'Home', hash: '#skills' }">
-                <button
-                    class="relative inline-block cursor-pointer outline-none border-none align-middle no-underline bg-transparent p-0 w-48 h-auto learn-more-button"
-                >
-                    <span
-                        class="relative block m-0 w-12 h-12 bg-dark dark:bg-primary rounded-full transition-all ease-[cubic-bezier(0.65, 0, 0.076, 1)] duration-[450ms] circle"
-                        aria-hidden="true"
-                    >
-                        <span class="icon arrow"></span>
-                    </span>
-                    <span
-                        class="absolute text-dark dark:text-primary font-jiho-regular uppercase text-cente button-text ml-10 leading-relaxed transition-all ease-[cubic-bezier(0.65, 0, 0.076, 1)] duration-[450ms] py-3 inset-0"
-                        >{{ $t("learn_more") }}</span
-                    >
-                </button>
-            </router-link>
+          <div class="w-8/12 sm:w-1/2">
+            <CTAButton
+              :selectedIcon="'download'"
+              :link="'/julie-gicquel/files/Julie-Gicquel-CV.pdf'"
+              :download="'Julie-Gicquel-CV.pdf'"
+              label="'CV'"
+              class="bg-red-600 dark:bg-primary hover:bg-red-700 dark:hover:bg-primary-darker text-white"
+            >
+              {{ $t("download_resume") }}
+            </CTAButton>
+          </div>
+          <div class="hidden sm:block sm:w-1/2 sm:ml-4">
+            <CTAButton
+              :selectedIcon="'mail'"
+              :link="`mailto:${mail}`"
+              label="'Contact'"
+              class="bg-white dark:bg-secondary dark:hover:bg-secondary-darker border-dark dark:border-secondary dark:hover:border-secondary-darker border-2 hover:bg-dark text-dark dark:text-white dark:hover:text-primary hover:text-white"
+            >
+              {{ $t("get_in_touch") }}
+            </CTAButton>
+          </div>
         </div>
+        <div class="flex flex-col space-y-2">
+          <p
+            class="font-jiho-medium text-lg xl:text-xl text-center lg:text-left text-primay-dark dark:text-white"
+          >
+            {{ $t("follow_me") }}
+          </p>
+          <div class="flex justify-center lg:justify-start">
+            <SocialButton />
+            <a
+              :href="`mailto:${mail}`"
+              class="inline-flex sm:hidden rounded-full h-11 w-11 sm:h-12 sm:w-12 items-center justify-center mx-2 bg-white dark:bg-secondary dark:hover:bg-secondary-darker border-dark dark:border-secondary dark:hover:border-secondary-darker border-2 hover:bg-dark text-dark dark:text-white dark:hover:text-primary hover:text-white text-center"
+            >
+              <i data-feather="mail" class="w-5 lg:w-16"></i>
+            </a>
+          </div>
+        </div>
+      </div>
+      <div class="w-full lg:w-6/12 flex justify-center lg:justify-end">
+        <img
+          v-if="theme === 'light'"
+          src="@/assets/images/hero3_LIGHT.png"
+          alt="Developer"
+          class="w-11/12 sm:w-7/12 lg:w-10/12"
+        />
+        <img
+          v-else
+          src="@/assets/images/hero3_LIGHT_darkmode.png"
+          alt="Developer"
+          class="w-11/12 sm:w-7/12 lg:w-10/12"
+        />
+      </div>
     </div>
+    <div class="hidden lg:flex lg:flex-1 flex-none items-center justify-center">
+      <router-link :to="{ name: 'Home', hash: '#skills' }">
+        <button
+          class="relative inline-block cursor-pointer outline-none border-none align-middle no-underline bg-transparent p-0 w-48 h-auto learn-more-button"
+        >
+          <span
+            class="relative block m-0 w-12 h-12 bg-dark dark:bg-primary rounded-full transition-all ease-[cubic-bezier(0.65, 0, 0.076, 1)] duration-[450ms] circle"
+            aria-hidden="true"
+          >
+            <span class="icon arrow"></span>
+          </span>
+          <span
+            class="absolute text-dark dark:text-primary font-jiho-regular uppercase text-cente button-text ml-10 leading-relaxed transition-all ease-[cubic-bezier(0.65, 0, 0.076, 1)] duration-[450ms] py-3 inset-0"
+            >{{ $t("learn_more") }}</span
+          >
+        </button>
+      </router-link>
+    </div>
+  </div>
 </template>
 <script>
-import feather from "feather-icons";
+  import feather from "feather-icons";
 
-import { mail } from "../data/socials";
+  import { mail } from "../data/socials";
 
-import SocialButton from "./reusable/SocialButton.vue";
-import CTAButton from "./reusable/CTAButton.vue";
+  import SocialButton from "./reusable/SocialButton.vue";
+  import CTAButton from "./reusable/CTAButton.vue";
 
-export default {
+  export default {
     components: { SocialButton, CTAButton },
     data() {
-        return {
-            theme: "",
-            mail,
-        };
+      return {
+        theme: "",
+        mail,
+      };
     },
     computed: {
-        heroParagraphs() {
-            return [
-                { id: 1, text: "hero_1" },
-                { id: 2, text: "hero_2" },
-            ];
-        },
+      heroParagraphs() {
+        return [
+          { id: 1, text: "hero_1" },
+          { id: 2, text: "hero_2" },
+        ];
+      },
     },
     created() {
-        this.theme = localStorage.getItem("theme") || "light";
+      this.theme = localStorage.getItem("theme") || "light";
     },
     mounted() {
-        feather.replace();
+      feather.replace();
     },
     updated() {
-        feather.replace();
+      feather.replace();
     },
-};
+  };
 </script>
 <style lang="css">
-button.learn-more-button .circle .icon {
+  button.learn-more-button .circle .icon {
     transition: all 0.45s cubic-bezier(0.65, 0, 0.076, 1);
     position: absolute;
     top: 0;
     bottom: 0;
     margin: auto;
     background: #fff;
-}
+  }
 
-button.learn-more-button .circle .icon.arrow {
+  button.learn-more-button .circle .icon.arrow {
     transition: all 0.45s cubic-bezier(0.65, 0, 0.076, 1);
     left: 0.625rem;
     width: 1.125rem;
     height: 0.125rem;
     background: none;
-}
+  }
 
-button.learn-more-button .circle .icon.arrow::before {
+  button.learn-more-button .circle .icon.arrow::before {
     position: absolute;
     content: "";
     top: -0.29rem;
@@ -168,18 +167,18 @@ button.learn-more-button .circle .icon.arrow::before {
     border-top: 0.125rem solid #fff;
     border-right: 0.125rem solid #fff;
     transform: rotate(45deg);
-}
+  }
 
-button:hover .circle {
+  button:hover .circle {
     width: 100%;
-}
+  }
 
-button:hover .circle .icon.arrow {
+  button:hover .circle .icon.arrow {
     background: #fff;
     transform: translate(1rem, 0);
-}
+  }
 
-button:hover .button-text {
+  button:hover .button-text {
     color: #fff;
-}
+  }
 </style>
