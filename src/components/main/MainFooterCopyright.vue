d689b2859206b680ebec1f55075f3a9d57c583a8
Julie <julie.gcql@gmail.com>
2026-03-29 11:31:46 +0200

    FIx: rename brand name

--- src/components/main/MainFooterCopyright.vue
+++ src/components/main/MainFooterCopyright.vue
@@ -1,47 +1,45 @@
 <template>
-    <div class="flex justify-center items-center text-center mb-4">
-        <div
-            class="font-jiho-regular text-lg text-grey-dark dark:text-grey-light"
-        >
-            <a
-                href="https://github.com/Mecha-Yabai"
-                target="__blank"
-                class="mr-1 font-jiho-medium text-grey-darker dark:text-white hover:text-red-500 dark:hover:text-primary duration-500"
-            >
-                {{ projectName }}
-            </a>
-            <span>{{ copyrightDate }}</span>
-            <br />
-            <div v-if="$i18n.locale === 'ja'">
-                <a
-                    :href="`mailto:${mail}`"
-                    class="font-jiho-medium text-grey-darker dark:text-white uppercase hover:text-red-500 dark:hover:text-primary duration-500"
-                    >{{ author }}
-                </a>
-                <span>{{ $t("footer.developed_by") }}</span>
-            </div>
-            <div v-else>
-                <span class="mr-1">{{ $t("footer.developed_by") }}</span>
-                <a
-                    :href="`mailto:${mail}`"
-                    class="font-jiho-medium text-grey-darker dark:text-white uppercase hover:text-red-500 dark:hover:text-primary duration-500"
-                    >{{ author }}.
-                </a>
-            </div>
-        </div>
+  <div class="flex justify-center items-center text-center mb-4">
+    <div class="font-jiho-regular text-lg text-grey-dark dark:text-grey-light">
+      <a
+        href="https://github.com/Meccha-Yabai"
+        target="__blank"
+        class="mr-1 font-jiho-medium text-grey-darker dark:text-white hover:text-red-500 dark:hover:text-primary duration-500"
+      >
+        {{ projectName }}
+      </a>
+      <span>{{ copyrightDate }}</span>
+      <br />
+      <div v-if="$i18n.locale === 'ja'">
+        <a
+          :href="`mailto:${mail}`"
+          class="font-jiho-medium text-grey-darker dark:text-white uppercase hover:text-red-500 dark:hover:text-primary duration-500"
+          >{{ author }}
+        </a>
+        <span>{{ $t("footer.developed_by") }}</span>
+      </div>
+      <div v-else>
+        <span class="mr-1">{{ $t("footer.developed_by") }}</span>
+        <a
+          :href="`mailto:${mail}`"
+          class="font-jiho-medium text-grey-darker dark:text-white uppercase hover:text-red-500 dark:hover:text-primary duration-500"
+          >{{ author }}.
+        </a>
+      </div>
     </div>
+  </div>
 </template>
 <script>
-export default {
+  export default {
     props: {
-        mail: String,
+      mail: String,
     },
     data() {
-        return {
-            copyrightDate: new Date().getFullYear(),
-            projectName: "Mecha Yabai©",
-            author: "Julie Gicquel",
-        };
+      return {
+        copyrightDate: new Date().getFullYear(),
+        projectName: "Meccha Yabai©",
+        author: "Julie Gicquel",
+      };
     },
-};
+  };
 </script>
--- src/data/socials.js
+++ src/data/socials.js
@@ -1,16 +1,16 @@
 export const socials = [
-    {
-        id: 1,
-        name: "Linkedin",
-        icon: "linkedin",
-        url: "https://www.linkedin.com/in/juliegicquel/",
-    },
-    {
-        id: 2,
-        name: "GitHub",
-        icon: "github",
-        url: "https://github.com/Mecha-Yabai",
-    },
+  {
+    id: 1,
+    name: "Linkedin",
+    icon: "linkedin",
+    url: "https://www.linkedin.com/in/juliegicquel/",
+  },
+  {
+    id: 2,
+    name: "GitHub",
+    icon: "github",
+    url: "https://github.com/Meccha-Yabai",
+  },
 ];
 
 export const mail = "julie.gcql@gmail.com";
